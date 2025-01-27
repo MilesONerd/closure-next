39a9904959fce9360cce358fa1509ac0b8ff5b8d
ℰ𝓃𝓏ℴ ℱ𝓊𝓀ℯ <150525657+MilesONerd@users.noreply.github.com>
2025-01-27 15:23:37 -0300

    Create string.d.ts

---
+++ packages/core/src/utils/string.d.ts
@@ -0,0 +1,25 @@
+/**
+ * @fileoverview String utilities for Closure Next.
+ * @license Apache-2.0
+ */
+/**
+ * Converts a string to camelCase
+ */
+export declare function toCamelCase(str: string): string;
+/**
+ * Converts a string to PascalCase
+ */
+export declare function toPascalCase(str: string): string;
+/**
+ * Compares two strings case-insensitively
+ */
+export declare function compareIgnoreCase(str1: string, str2: string): number;
+/**
+ * HTML-escapes a string
+ */
+export declare function htmlEscape(str: string): string;
+/**
+ * Checks if a string contains another string, case-insensitively
+ */
+export declare function containsIgnoreCase(str: string, substr: string): boolean;
+//# sourceMappingURL=string.d.ts.map
