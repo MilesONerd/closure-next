191b0dea865e40d77d80668126fff1f77a94458d
ℰ𝓃𝓏ℴ ℱ𝓊𝓀ℯ <150525657+MilesONerd@users.noreply.github.com>
2025-01-27 15:01:03 -0300

    Create index.js

---
+++ packages/core/src/index.js
@@ -0,0 +1,10 @@
+/**
+ * @fileoverview Entry point for Closure Next core functionality.
+ * @license Apache-2.0
+ */
+export * from './component';
+export * from './events';
+export * from './dom';
+export * from './id';
+export * from './utils';
+//# sourceMappingURL=index.js.map
