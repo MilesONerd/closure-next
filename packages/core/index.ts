b568bf14b6f20395c2085a6ae9cb5e3d42cb76c8
ℰ𝓃𝓏ℴ ℱ𝓊𝓀ℯ <150525657+MilesONerd@users.noreply.github.com>
2025-01-27 14:59:31 -0300

    Create index.ts

---
+++ packages/core/index.ts
@@ -0,0 +1,14 @@
+/**
+ * @fileoverview Entry point for Closure Next core functionality.
+ * @license Apache-2.0
+ */
+
+// Re-export everything at the root level
+export * from './component';
+export * from './events';
+export * from './dom';
+export * from './id';
+export * from './utils';
+
+// Export types
+export type { ComponentInterface } from './component';
