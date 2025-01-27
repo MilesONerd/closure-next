664d80288fa1cb863b4c1ea2d0ab082d7908f65a
ℰ𝓃𝓏ℴ ℱ𝓊𝓀ℯ <150525657+MilesONerd@users.noreply.github.com>
2025-01-27 16:16:50 -0300

    Create jest.config.js

---
+++ src/integrations/angular/jest.config.js
@@ -0,0 +1,44 @@
+/** @type {import('@jest/types').Config.InitialOptions} */
+module.exports = {
+  preset: 'jest-preset-angular',
+  testEnvironment: 'jsdom',
+  roots: ['<rootDir>/src/__tests__'],
+  transform: {
+    '^.+\\.(ts|js|mjs|html|svg)$': [
+      'jest-preset-angular',
+      {
+        tsconfig: '<rootDir>/tsconfig.test.json',
+        isolatedModules: true,
+        stringifyContentPathRegex: '\\.html$',
+        diagnostics: { warnOnly: true }
+      }
+    ]
+  },
+  moduleDirectories: [
+    'node_modules',
+    '<rootDir>/node_modules',
+    '<rootDir>/../../node_modules',
+    '<rootDir>/../../../node_modules'
+  ],
+  moduleNameMapper: {
+    '^@closure-next/core$': '<rootDir>/../../../packages/core/src'
+  },
+  setupFilesAfterEnv: ['<rootDir>/jest.setup.js'],
+  testPathIgnorePatterns: ['/node_modules/'],
+  moduleFileExtensions: ['ts', 'html', 'js', 'json', 'mjs'],
+  transformIgnorePatterns: [
+    'node_modules/(?!(@angular|tslib|rxjs))'
+  ],
+  testEnvironmentOptions: {
+    customExportConditions: ['node', 'node-addons']
+  },
+  globals: {
+    'ts-jest': {
+      tsconfig: '<rootDir>/tsconfig.test.json',
+      diagnostics: false
+    }
+  },
+  verbose: true,
+  testTimeout: 30000,
+  maxWorkers: 1
+};
