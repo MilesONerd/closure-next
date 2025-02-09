622b62449d0b3c779ac27d24b13b581a8a6f9a25
ℰ𝓃𝓏ℴ ℱ𝓊𝓀ℯ <150525657+MilesONerd@users.noreply.github.com>
2025-02-08 23:12:48 -0300

    Create conf.py

---
+++ docs/source/conf.py
@@ -0,0 +1,10 @@
+project = 'Closure Next Documentation'
+author = 'MilesONerd'
+release = '1.0'
+
+extensions = []
+
+templates_path = ['_templates']
+
+source_suffix = '.rst'  
+master_doc = 'index'
